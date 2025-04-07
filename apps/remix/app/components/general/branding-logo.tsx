525f4e80a2563fcf1c53dc01900e222b2ffafe9b
dorklein <dorklein2@gmail.com>
2025-04-07 16:54:47 +0300

    fix: enhance branding-logo component with dark mode support

--- apps/remix/app/components/general/branding-logo.tsx
+++ apps/remix/app/components/general/branding-logo.tsx
@@ -1,7 +1,14 @@
 import LogoImage from '@documenso/assets/logo.png';
+import { cn } from '@documenso/ui/lib/utils';
 
 export const BrandingLogo = (props: { className?: string }) => {
   return (
-    <img src={LogoImage} alt="Smartsign Logo" className={props.className} width={170} height={25} />
+    <img
+      src={LogoImage}
+      alt="Smartsign Logo"
+      className={cn('dark:invert', props.className)}
+      width={170}
+      height={25}
+    />
   );
 };
