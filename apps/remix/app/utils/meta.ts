5ce42c258cd5ceb9cf4412791e7ec47ed5d873cf
dorklein <dorklein2@gmail.com>
2025-05-31 15:34:46 +0300

    feat: enhance SigningCertificate component with logo and translation support
    - Added logo image to the SigningCertificate component for
    branding.
    - Implemented translation for "Signed via software by" in
    multiple languages.
    - Improved date formatting consistency for document
    timestamps and logs.

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -2,7 +2,7 @@ import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Join Smartsign, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
+    'Join Smartsign, the open signing infrastructure, and get a 10x better signing experience.';
 
   return [
     {
@@ -15,7 +15,7 @@ export const appMetaTags = (title?: string) => {
     {
       name: 'keywords',
       content:
-        'Smartsign, open source, DocuSign alternative, document signing, open signing infrastructure, open-source community, fast signing, beautiful signing, smart templates',
+        'Smartsign, DocuSign alternative, document signing, open signing infrastructure, open-source community, fast signing, beautiful signing, smart templates',
     },
     {
       name: 'author',
@@ -27,7 +27,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'Smartsign - The Open Source DocuSign Alternative',
+      content: 'Smartsign - The DocuSign Alternative',
     },
     {
       property: 'og:description',
